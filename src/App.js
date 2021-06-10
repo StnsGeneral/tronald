5baac95a255250317855aad74e202648a7ca89e9
StnsGeneral <tristanmiller89@gmail.com>
2021-06-10 02:29:37 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import AddForm from './components/AddForm';
+import SmurfList from './components/SmurfList';
+import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import { fetchSmurfs } from './actions';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchSmurfs();
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+
+        <main>
+          <SmurfList />
+          <AddForm />
+        </main>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = { fetchSmurfs };
+
+export default connect(null, mapStateToProps)(App);
+
+//Task List:
+//1. Connect the fetchSmurfs actions to the App component.
+//2. Call the fetchSmurfs action when the component first loads.
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import './index.css';
+import App from './App';
+
+import { reducer } from './reducers';
+
+const { worker } = require('./mocks/browser');
+worker.start();
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
+
+//Task List:
+//1. Add in all necessary components and libary methods.
+//2. Create a store that includes thunk and logger middleware support.
+//3. Wrap the App component in a react-redux Provider element.
